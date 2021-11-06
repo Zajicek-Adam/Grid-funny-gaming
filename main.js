4cf7d3e257a8b7de1602bae60da04b70f1b84f7a
Zeidash <adam.zajicek.020@pslib.cz>
2021-11-06 19:14:03 +0100

    Finally fixed refreshing on resize

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ let defaultsize = 150;
 let bg = document.getElementById("bg");
 let img = document.getElementById("img");
 let header = document.getElementById("h");
-window.onresize = function(){ setTimeout(function(){
-    window.location.reload();
-  }); }
 function handleMediaQuery(x){
     if(x.matches){
         isBigEnough = false;
